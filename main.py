a762947c9d73d74fc3a5bd92d7a65926bd5958cb
stephantheartist <ptalexrosu@gmail.com>
2021-01-21 09:40:31 +0200

    mi first project commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# # my_strings = ['ana', 'are', 'mere', [1, 2, 3 ,['a', 'b', 'c']]
+# # # print(type(my_strings))
+# # # print(type(my_strings).__name__)
+# # # print (my_strings)
+# # # print(len(my_strings))
+# # # print(my_strings[0])
+# # # print(my_strings[1])
+# # # print(my_strings[2])
+# # # print(my_strings[len(my_strings)-1])
+# # # print(my_strings[len(my_strings)-2])
+# # # print(my_strings[len(my_strings)-3])
+# # # print(my_strings[len(my_strings)-4])
+# # # print(my_strings[len(my_strings)-5])
+# # # print(my_strings[len(my_strings)-6])
+# # # print(my_strings[3][1])
+# # print(my_strings[3][3][2])
+# my_fruits = ['banane', 'mere', 'gutui', 'pere','capsuni','pomelo','lubenita']
+# print(my_fruits)
+#
+# new_list = my_fruits[1:-1:3]  # start_index : end_index : step
+# print(new_list)
+#
+# a = 256
+# b = 256
+# print(a is b)
+#
+# c = 257
+# d = 257
+# print('3')
+
+my_tuple = ('a', 'b', 'c')
+print(my_tuple)
+print(type)my_tuple))
