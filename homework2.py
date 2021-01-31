c12b8d592d13eae7759dbab1450b7eed5e024c7b
stephantheartist <ptalexrosu@gmail.com>
2021-01-31 10:52:39 +0200

    homework2

---
+++ homework2.py
@@ -0,0 +1,55 @@
+#
+# def get_sum (a, b, c):
+#     return a + b + c
+# my_sum = get_sum (1, 5, -3)
+# print('Result', my_sum)
+
+# def get_sum (a, b, c):
+#     return 0
+# my_sum = get_sum (1, 5, -3)
+# print('Result', my_sum)
+
+# def my_function(a, b, param_1, param_2 ):
+#     return a + b + param_1 + param_2
+#
+# my_sum = my_function (2, 4, 0, 0  )
+# print(my_sum)
+
+#
+# def get_sum(n):
+#     if n == 0:
+#         return 0
+#     return n + get_sum (n - 1)
+# print(get_sum(10))
+
+# def get_sum(n):
+#     if n == 0:
+#         return 0
+#     return n + get_sum (n - 2)
+# print(get_sum(8))
+
+# def get_sum(n):
+#     if n == 0:
+#         return 0
+#     return n + get_sum (n - 3)
+# print(get_sum(3))
+
+n = int(input('Pick a number:'))
+
+if n == int(n):
+
+    # print(str(n)  + " " + 'Yes this is a whole number !')
+    print('Yes this is a whole number !')
+
+
+
+
+
+
+
+
+
+
+
+
+
